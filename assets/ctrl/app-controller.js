7b4510bb59c25b5a6dc7b181ea9c6308e2560130
nghitruong <89492293+vntruongdev@users.noreply.github.com>
2022-10-27 00:00:47 +0700

    Xong tinh nang xoa cart item
    Luu cart vao localStorage, xoa cart item tren navbar, xoa cart item trong cart page

--- assets/ctrl/app-controller.js
+++ assets/ctrl/app-controller.js
@@ -71,26 +71,68 @@ app.factory("myService", function () {
 });
 
 app.controller("myCtrl", ($scope, $rootScope) => {
-  $rootScope.cart = new Map();
+  $rootScope.cart = {
+    map: new Map(),
+    array: [],
+    total: 0,
+  };
+  getCartFromLocal($rootScope.cart);
 
   $scope.$on("addCart", (evt, data) => {
     let cart = $rootScope.cart;
-    if (cart.has(data.id)) {
-      let item = cart.get(data.id);
-      item.quantity++;
-    } else {
-      cart.set(data.id, data);
-    }
+    addToCart(cart, data);
+  });
 
-    $scope.total = getTotal(cart);
-    $scope.cartArr = [...cart.values()];
+  $scope.removeCartItem = (item) => {
+    console.log("remove item", item);
+    removeItem($rootScope.cart, item);
+  };
+
+  $scope.$on("removeCartItem", (evt, data) => {
+    console.log("remove cart item in app controller");
+    $scope.removeCartItem(data);
   });
 });
 
-let getTotal = function (cart) {
+function addToCart(cart, product) {
+  if (cart.map.has(product.id)) {
+    let item = cart.map.get(product.id);
+    item.quantity++;
+  } else {
+    cart.map.set(product.id, product);
+  }
+  loadCartData(cart);
+}
+
+function getTotal(cart) {
   let total = 0;
   cart.forEach((item) => {
     total += item.price * item.quantity;
   });
   return total;
-};
+}
+
+function saveToLocal(cart) {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function loadCartData(cart) {
+  cart.total = getTotal(cart.map);
+  cart.array = [...cart.map.values()];
+  saveToLocal(cart.array);
+}
+
+function getCartFromLocal(cart) {
+  let items = JSON.parse(localStorage.getItem("cart"));
+  if (items) {
+    items.forEach((item) => cart.map.set(item.id, item));
+    loadCartData(cart);
+  }
+}
+
+function removeItem(cart, item) {
+  if (cart.map.has(item.id)) {
+    console.log(cart.map.delete(item.id));
+    loadCartData(cart);
+  }
+}
