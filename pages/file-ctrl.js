ab1e20b7070bc0b649d948b3e38b6a8742581e18
phidhps18381 <phidhps18381@fpt.edu.vn>
2022-11-09 10:58:26 +0700

    Load images

--- pages/file-ctrl.js
+++ pages/file-ctrl.js
@@ -1,5 +1,21 @@
 var app = angular.module("app", []);
-app.controller("ctrl", function ($scope, $http) {
+
+
+app.factory('imgService', function () {
+    var savedData = {}
+    function set(data) {
+        savedData = data;
+    }
+    function get() {
+        return savedData;
+    }
+    return {
+        set: set,
+        get: get
+    }
+});
+
+app.controller("ctrl", function ($scope, $http, imgService, $interval) {
     // var url = `http://localhost:8082/rest/files/images`
     // $scope.url = function (filename) {
     //     return `${url}/${filename}`;
@@ -57,6 +73,69 @@ app.controller("ctrl", function ($scope, $http) {
     // //
     // $scope.list();
 
+    var url = "http://localhost:8082/rest/images";
+    $scope.items = [];
+    $scope.imgdata1 = imgService.get();
+
+    //load data
+    $http.get(url).then((result) => {
+        $scope.items = result.data;
+    }).catch((err) => {
+        console.log("Error", err)
+    })
+
+    //xoa form
+    $scope.reset = function () {
+        $scope.imgdata1 = {};
+    }
+
+    //hien thi len form
+    $scope.edit = function (item) {
+        $scope.imgdata = angular.copy($scope.items[item])
+        imgService.set($scope.imgdata)
+        $scope.imgdata1 = imgService.get()
+        $scope.updateImg();
+    }
+
+    //them moi
+    $scope.create = function () {
+        $scope.imgdata1.image = document.getElementById("imgs").value
+        var item = angular.copy($scope.imgdata1);
+        $http.post(`${url}`, item).then(resp => {
+            $scope.items.push(resp.data);
+            $scope.reset();
+        }).catch(error => {
+            console.log("Error", error);
+        });
+    }
+
+    //cap nhat sp
+    $scope.update = function () {
+        var item = angular.copy($scope.imgdata1);
+        $http.put(`${url}/${item.stt}`, item).then(resp => {
+            var index = $scope.items.findIndex(p => p.stt == item.stt);
+            $scope.items[index] = item;
+            $scope.reset();
+        }).catch(error => {
+            console.log("Error", error);
+        });
+    }
+
+    //xoa sp
+    $scope.delete = function () {
+        $http.delete(`${url}/${$scope.imgdata1.stt}`).then(resp => {
+            var index = $scope.items.findIndex(p => p.stt == $scope.imgdata1.stt);
+            $scope.items.splice(index, 1);
+            $scope.reset();
+        }).catch(error => {
+            console.log("Error", error);
+        });
+    }
+
+    // $scope.changeImg = function () {
+    //     document.getElementById("img").value = $scope.imgdata1.image
+    // }
+
     $('document').ready(function () {
         $('input[type=file]').on('change', function () {
             var $files = $(this).get(0).files;
@@ -68,7 +147,7 @@ app.controller("ctrl", function ($scope, $http) {
                     }
                 }
                 console.log('Đang upload hình ảnh lên imgur...');
-
+                const arr = [];
                 for (let index = 0; index < $files.length; index++) {
                     var apiUrl = 'https://api.imgur.com/3/image';
                     var apiKey = '146def7f79c7a87';
@@ -93,7 +172,7 @@ app.controller("ctrl", function ($scope, $http) {
                         console.log('done');
                         var obj = JSON.parse(response);
                         var cut = JSON.stringify(obj.data.link);
-                        // img.push(cut.slice(1, cut.length - 1));
+                        arr.push(cut.slice(1, cut.length - 1));
                         const para = document.createElement("img");
                         para.setAttribute("src", cut.slice(1, cut.length - 1));
                         para.setAttribute("referrerpolicy", "no-referrer");
@@ -101,9 +180,29 @@ app.controller("ctrl", function ($scope, $http) {
                         document.getElementById("myDIV").appendChild(para);
                     });
                 }
+                console.log(arr.toString().split(','))
+                document.getElementById("imgs").value = arr.toString().split(',')
+                console.log(document.getElementById("imgs").value)
             }
-            // img.forEach(element => {
-            // });
         });
     });
-})
+    $scope.updateImg = () => {
+        var listImg = document.getElementById("imgs").value.split(',');
+        for (let index = 0; index < listImg.length; index++) {
+            const para = document.createElement("img");
+            para.setAttribute("src", listImg[index]);
+            para.setAttribute("referrerpolicy", "no-referrer");
+            para.style.width = '300px';
+            document.getElementById("myDIV").appendChild(para);
+        }
+    }
+
+    // Load n images
+    // if (document.getElementById("imgs").value == "") {
+    //     console.log("Loading images...");
+    // } else {
+
+    //     console.log("Imag")
+    // }
+    // console.log(document.getElementById("imgs").value)
+});
