e83cfdc9addcd8f5ba95dc83be7eef43114f6234
phidhps18381 <phidhps18381@fpt.edu.vn>
2022-11-11 09:48:00 +0700

    lại tè le nữa r :(((

--- pages/file-ctrl.js
+++ pages/file-ctrl.js
@@ -16,63 +16,6 @@ app.factory('imgService', function () {
 });
 
 app.controller("ctrl", function ($scope, $http, imgService, $interval) {
-    // var url = `http://localhost:8082/rest/files/images`
-    // $scope.url = function (filename) {
-    //     return `${url}/${filename}`;
-    // }
-
-    // $scope.list = function () {
-    //     $http.get(url).then((result) => {
-    //         $scope.filenames = result.data;
-    //     }).catch((err) => {
-    //         console.log("Error", err)
-    //     });
-    // }
-
-    // $scope.nameFile = function (e) {
-    //     for (let index = 0; index < e.length; index++) {
-    //         console.log(e[index].name);
-    //         document.getElementById("img-text").innerText = e[index].name;
-    //     }
-    //     var arrM = [...e];
-    //     var arm = arrM.map(function (ele, index) {
-    //         return `${ele.name}`
-    //     })
-    //     console.log(arm.join(","))
-    // }
-
-    // $scope.upload = function (files) {
-    //     var form = new FormData();
-    //     for (var i = 0; i < files.length; i++) {
-    //         form.append("files", files[i]);
-    //     }
-
-    //     $http.post(url, form, {
-    //         transformRequest: angular.identity,
-    //         headers: { 'Content-Type': undefined }
-    //     }).then((result) => {
-    //         $scope.filenames.push(...result.data);
-    //         $scope.list();
-    //     }).catch((err) => {
-    //         console.log("Errors1", err)
-    //     });
-    //     // $scope.nameFile(files);
-    //     // console.log(Date.now())
-    // }
-
-    // $scope.delete = function (filename) {
-    //     $http.delete(`${url}/${filename}`).then((result) => {
-    //         let i = $scope.filenames.findIndex(name => name == filename);
-    //         $scope.filenames.splice(i, 1);
-    //         $scope.list();
-    //     }).catch((err) => {
-    //         console.log("Error", err)
-    //     });
-    //     // $scope.nameFile();
-    // }
-    // //
-    // $scope.list();
-
     var url = "http://localhost:8082/rest/images";
     $scope.items = [];
     $scope.imgdata1 = imgService.get();
@@ -196,6 +139,10 @@ app.controller("ctrl", function ($scope, $http, imgService, $interval) {
             document.getElementById("myDIV").appendChild(para);
         }
     }
+
+    $(".btnEdit").click(() => {
+        alert("Please enter a")
+    })
 
     // Load n images
     // if (document.getElementById("imgs").value == "") {
