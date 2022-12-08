1e7e006bbdae5ec12f983b8db8f67fb3ae4e758a
HuyPhi <phidhps18381@fpt.edu.vn>
2022-12-08 22:08:27 +0700

    add create product (user) without check form

--- assets/ctrl/user-controller.js
+++ assets/ctrl/user-controller.js
@@ -1,6 +1,8 @@
 "use strict";
 app.controller("user-ctrl", function ($scope, $rootScope, $location, $utility, HOST, userService, authService) {
     const { $http, $data, $serverUrl, $message } = $utility;
+
+    var urlProd = "http://localhost:8080/api/product";
     $scope.items = [];
     $scope.userdata = {};
 
@@ -17,23 +19,23 @@ app.controller("user-ctrl", function ($scope, $rootScope, $location, $utility, H
     $scope.product = {};
     $scope.productByUserId = [];
 
+    $scope.cate = {};
+    $scope.company = {};
+
     $http.get(HOST + "/api/user").then((resp) => {
         $scope.items = resp.data;
+    });
+
+    $http.get(HOST + "/api/category").then((resp) => {
+        $scope.cates = resp.data;
     });
 
     $http.get(HOST + "/api/company").then((resp) => {
         $scope.companys = resp.data;
     });
-    $http.get(HOST + "/api/category").then((resp) => {
-        $scope.cates = resp.data;
-    });
 
     $http.get(HOST + "/api/product").then((resp) => {
         $scope.products = resp.data;
-    });
-
-    $http.get(HOST + "/api/product/userid/11").then((resp) => {
-        $scope.productByUserId = resp.data;
     });
 
     function handleRequest(res) {
@@ -44,6 +46,18 @@ app.controller("user-ctrl", function ($scope, $rootScope, $location, $utility, H
         }
         $scope.message = res.data.message;
     }
+
+    // $scope.findUser = function () {
+    //     $scope.product.user = $scope.items.filter(function (item) {
+    //         return item.username === $scope.productdata.user.username;
+    //     })[0];
+    // }
+
+    setTimeout(() => {
+        $scope.product.user = $scope.items.filter(function (item) {
+            return item.username == localStorage.getItem('currentUser');
+        })[0];
+    }, 200)
 
     $scope.login = function () {
         userService
@@ -68,11 +82,18 @@ app.controller("user-ctrl", function ($scope, $rootScope, $location, $utility, H
     };
 
     $scope.reset = function () {
-        $scope.userdata = {};
+        $scope.product = {};
+
+        // * Delete all element in class "myDIV"
+        setTimeout(() => {
+            const getdiv = document.getElementById("myDIV");
+            getdiv.innerHTML = "";
+        }, 1000)
     };
 
     $scope.updateInfo = function () {
         var item = angular.copy($scope.userdata);
+        console.log(item)
         $http.put(`${HOST}/api/user/${item.id}`, item).then((resp) => {
             var index = $scope.items.findIndex((p) => p.id == item.id);
             $scope.items[index] = item;
@@ -97,43 +118,99 @@ app.controller("user-ctrl", function ($scope, $rootScope, $location, $utility, H
         });
     };
 
-    $("#btnAddProd").click(() => {
-        // $scope.product.createdAt = moment().format('YYYY-MM-DD HH:mm');
-        // $scope.product.updatedAt = moment().format('YYYY-MM-DD HH:mm');
-        // $scope.product.user = 11;
-        // $scope.product.image = "null.png";
-
-        // var item = angular.copy($scope.product);
-        // $http.post(HOST + "/api/product", item).then(resp => {
-        //     $scope.products.push(resp.data);
-        //     $scope.reset();
-        // }).catch(err => {
-        //     console.log("Error", err)
-        // })
-    })
-
     $scope.createProd = () => {
         $scope.product.createdAt = moment().format('YYYY-MM-DD HH:mm');
         $scope.product.updatedAt = moment().format('YYYY-MM-DD HH:mm');
-        $scope.product.user = 11;
-        $scope.product.image = "null.png";
-        $scope.product.available = true;
+        $scope.product.category = $scope.cates[$scope.product.category - 1];
+        $scope.product.company = $scope.companys[$scope.product.company - 1];
+        $scope.product.image = document.getElementById("imgs").value;
+        $scope.product.available = false;
 
         var item = angular.copy($scope.product);
-        $http.post(`${"http://localhost:8080/api/product"}`, item).then(resp => {
-            // $scope.products.push(resp.data);
-            // $scope.reset();
+        console.log(item)
+        $http.post(`${urlProd}`, item).then(resp => {
+            $scope.products.push(resp.data);
+            $scope.reset();
+            alert("Created product success!!")
         }).catch(err => {
             console.log("Error", err)
         })
+    };
+
+    // list IMG
+    $scope.updateImg = () => {
+        var listImg = document.getElementById("imgs").value.split(',');
+        for (let index = 0; index < listImg.length; index++) {
+            const outImg = document.createElement("div");
+            outImg.setAttribute("class", "outImg")
+            const img = document.createElement("img");
+            para.setAttribute("src", listImg[index]);
+            img.setAttribute("ondblclick", `angular.element(this).scope().deleteImg(` + index + `)`);
+            img.setAttribute("referrerpolicy", "no-referrer");
+            img.style.width = '300px';
+            outImg.appendChild(img);
+            document.getElementById("myDIV").appendChild(outImg);
+        }
     }
 
-    // $scope.equa = function(prop, val) {
-    //     return function(item) {
-    //         console.log(item[prop])
-    //         return item[prop] == val;
-    //     }
-    // }
+    // Upload IMG to imgur api
+    $('document').ready(function () {
+        $('input[type=file]').on('change', function () {
+            var $files = $(this).get(0).files;
+            const getdiv = document.getElementById("myDIV");
+            getdiv.innerHTML = "";
+            if ($files.length) {
+                for (let index = 0; index < $files.length; index++) {
+                    if ($files[index].size > $(this).data('max-size') * 1024) {
+                        console.log('Vui lòng chọn file có dung lượng nhỏ hơn!');
+                        return false;
+                    }
+                }
+                document.getElementById("imgs").value = '';
+                console.log('Đang upload hình ảnh lên imgur...');
+                const arr = [];
+                for (let index = 0; index < $files.length; index++) {
+                    var apiUrl = 'https://api.imgur.com/3/image';
+                    var apiKey = '146def7f79c7a87';
+                    var settings = {
+                        async: false,
+                        crossDomain: true,
+                        processData: false,
+                        contentType: false,
+                        type: 'POST',
+                        url: apiUrl,
+                        headers: {
+                            Authorization: 'Client-ID ' + apiKey,
+                            Accept: 'application/json',
+                        },
+                        mimeType: 'multipart/form-data',
+                    };
+                    var formData = new FormData();
+                    formData.append('image', $files[index]);
+                    settings.data = formData;
+
+                    $.ajax(settings).done(function (response) {
+                        console.log('done');
+                        var obj = JSON.parse(response);
+                        var cut = JSON.stringify(obj.data.link);
+                        arr.push(cut.slice(1, cut.length - 1));
+                        const outImg = document.createElement("div");
+                        outImg.setAttribute("class", "outImg")
+                        const img = document.createElement("img");
+                        img.setAttribute("src", cut.slice(1, cut.length - 1));
+                        img.setAttribute("ondblclick", `angular.element(this).scope().deleteImg(` + index + `)`);
+                        img.setAttribute("referrerpolicy", "no-referrer");
+                        img.style.width = '300px';
+                        outImg.appendChild(img);
+                        document.getElementById("myDIV").appendChild(outImg);
+                    });
+                }
+                console.log(arr.toString().split(','))
+                document.getElementById("imgs").value = arr.toString().split(',')
+                console.log(document.getElementById("imgs").value)
+            }
+        });
+    });
 });
 
 app.controller("reset-password-ctrl", function ($scope, $utility) {
