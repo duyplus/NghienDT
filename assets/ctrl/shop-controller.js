79945e9b6e2feeeae501f3bbf77fbba76c4c041e
HuyPhi <phidhps18381@fpt.edu.vn>
2022-12-08 21:35:15 +0700

    Comple shop without filter

--- assets/ctrl/shop-controller.js
+++ assets/ctrl/shop-controller.js
@@ -1,3 +1,46 @@
-app.controller('shop-ctrl', function ($scope, $location, $http) {
-    
+app.controller('shop-ctrl', function ($scope, $filter, $http, HOST, $cart, $product, $utility) {
+    const { $data, $url, $templateUrl } = $utility;
+
+    $scope.templateUrl = $templateUrl.getHomeTemplates();
+    // Paging
+    $scope.products = [];
+
+    $http.get(HOST + "/api/product").then((resp) => {
+        $scope.products = resp.data;
+    });
+
+    $scope.addCart = (product) => {
+        $cart.addItem(product);
+    };
+
+    $scope.viewProduct = (event, product) => {
+        event.preventDefault();
+        $product.current = product;
+        $url.redirectToProductPage();
+    };
+
+    $scope.currentPage = 0;
+    $scope.pageSize = 12;
+    $scope.q = '';
+
+    $scope.getData = function () {
+        return $filter('filter')($scope.products, $scope.q)
+    }
+
+    $scope.numberOfPages = function () {
+        return Math.ceil($scope.products.length / $scope.pageSize);
+    }
+
+    // setTimeout(() => {
+    //     $(document).ready(function () {
+    //         if ($scope.currentPage == 0) {
+    //             console.log('1')
+    //         } else {
+    //             console.log('2')
+    //         }
+    //         $("#btnPreShop").click(function () {
+    //             console.log("sd")
+    //         })
+    //     })
+    // }, 1000)
 });
