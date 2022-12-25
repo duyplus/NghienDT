a541ce5661b1224bf23e8bac99ebf6eda5c3972e
HuyPhi <phidhps18381@fpt.edu.vn>
2022-12-25 15:09:23 +0700

    add alert error

--- assets/ctrl/checkout-controller.js
+++ assets/ctrl/checkout-controller.js
@@ -21,6 +21,14 @@ app.controller("checkout-ctrl", function ($scope, HOST, $cart, $http, $window, a
         });
     }
 
+    var sweetalert_error = function (text) {
+        Swal.fire({
+            icon: "error",
+            title: text,
+            showConfirmButton: false,
+            timer: 3000,
+        });
+    }
 
     $scope.isAuthed = function () {
         return authService.isAuthed ? authService.isAuthed() : false;
@@ -39,52 +47,58 @@ app.controller("checkout-ctrl", function ($scope, HOST, $cart, $http, $window, a
     });
 
     $scope.checkout = async () => {
-        // * POST Order
-        var datetime = new Date();
-        $scope.order.createdat = datetime.getTime();
-        $scope.order.updatedat = datetime.getTime();
-        // * GET user
-        $scope.order.user = $scope.userid;
+        if ($scope.check == false || $scope.check == undefined) {
+            sweetalert_error("Vui lòng đọc các điều khoản dịch vụ và tích vào ô để thực hiện thanh toán")
+        } else {
+            // * POST Order
+            var datetime = new Date();
+            $scope.order.createdat = datetime.getTime();
+            $scope.order.updatedat = datetime.getTime();
+            // * GET user
+            $scope.order.user = $scope.userid;
 
-        var item = angular.copy($scope.order);
-        console.log(item);
-        await $http.post(`${urlorder}`, item).then((resp) => {
-            $scope.orders.push(resp.data);
-            $scope.order.id = resp.data.id;
-        });
-        
+            var item = angular.copy($scope.order);
+            console.log(item);
+            await $http.post(`${urlorder}`, item).then((resp) => {
+                $scope.orders.push(resp.data);
+                $scope.order.id = resp.data.id;
+            });
 
-        // * Recover Order
-        var item2 = angular.copy($scope.order);
-        // * POST orderdetail
-        for (const cart of $cart.values) {
-            $scope.orderdetail.price = cart.price;
-            $scope.orderdetail.quantity = cart.quantity;
-            $scope.orderdetail.status = '1';
-            $scope.orderdetail.order = item2;
-            $scope.orderdetail.product = cart;
-            $scope.orderdetail.reviewed = false;
 
-            var itemoddt = angular.copy($scope.orderdetail);
-            console.log(itemoddt);
-            await $http.post(`${urlorderdetail}`, itemoddt).then(response => {
-                $scope.orderdetails.push(response.data);
-            })
-            //subtract quantity prod
-            var product = {}
-            await $http.get(urlprod + '/' + cart.id).then(resp => {
-                product = resp.data;
-            })
-            product.quantity = (product.quantity - cart.quantity);
-            var subtracted = product;
-            await $http.put(urlprod + '/' + cart.id, subtracted).then(resp => {
-                console.log(resp.data);
-            })
+            // * Recover Order
+            var item2 = angular.copy($scope.order);
+            // * POST orderdetail
+            for (const cart of $cart.values) {
+                $scope.orderdetail.price = cart.price;
+                $scope.orderdetail.quantity = cart.quantity;
+                $scope.orderdetail.status = '1';
+                $scope.orderdetail.order = item2;
+                $scope.orderdetail.product = cart;
+                $scope.orderdetail.reviewed = false;
+
+                var itemoddt = angular.copy($scope.orderdetail);
+                console.log(itemoddt);
+                await $http.post(`${urlorderdetail}`, itemoddt).then(response => {
+                    $scope.orderdetails.push(response.data);
+                })
+                //subtract quantity prod
+                var product = {}
+                await $http.get(urlprod + '/' + cart.id).then(resp => {
+                    product = resp.data;
+                })
+                product.quantity = (product.quantity - cart.quantity);
+                var subtracted = product;
+                await $http.put(urlprod + '/' + cart.id, subtracted).then(resp => {
+                    console.log(resp.data);
+                })
+            }
+            sweetalert_success("Thanh toán thành công")
+            localStorage.removeItem("cart");
+            setTimeout(() => {
+                $window.location.href = 'http://127.0.0.1:5500/#!/my-account';
+                window.location.reload();
+            }, 2500)
         }
-        sweetalert_success("Thanh toán thành công")
-        localStorage.removeItem("cart");
-        $window.location.href = 'http://127.0.0.1:5500/#!/';
-        window.location.reload();
     }
 
     // Get info user
